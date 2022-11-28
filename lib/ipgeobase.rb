219ae466fb08689a26f35d5f9dfdd50c2c13b62b
AntonLettuce <antonlettuce@gmail.com>
2022-11-28 23:01:25 +0400

    adding new functionality

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
+require "addressable/uri"
+require 'net/http'
+require 'happymapper'
 
 module Ipgeobase
   class Error < StandardError; end
   # Your code goes here...
+  def self.lookup ip
+    url = Addressable::URI.parse("http://ip-api.com/xml/#{ip}?fields=211")
+    response = Net::HTTP.get(url.normalize)
+    HappyMapper.parse(response)
+  end
 end
--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -1,13 +1,32 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require 'net/http'
+require_relative "../lib/ipgeobase/version"
 
 class TestIpgeobase < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Ipgeobase::VERSION
+  def setup
+    @ip = '83.169.216.199'
+    @uri = "http://ip-api.com/xml/#{@ip}?fields=211"
+    stub_request(:get, @uri)
+      .to_return(body:
+      '<?xml version="1.0" encoding="UTF-8"?>
+      <query>
+        <country>Russia</country>
+        <countryCode>RU</countryCode>
+        <city>Yekaterinburg</city>
+        <lat>56.8439</lat>
+        <lon>60.6524</lon>
+      </query>')
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_for_getting_something
+    result = Ipgeobase.lookup(URI(@ip));
+    assert_equal false, result.nil?
+  end
+
+  def test_for_existing_property
+    result = Ipgeobase.lookup(URI(@ip));
+    assert_equal 'Russia', result.country
   end
 end
